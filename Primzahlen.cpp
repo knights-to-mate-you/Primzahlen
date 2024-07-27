721fd26bcf77b07a3f4bf8a533421eefc450d11f
knights-to-mate-you <knights_to_mate_you@gmx.net>
2024-07-27 14:40:23 +0200

    Update Primzahlen.cpp

--- Primzahlen.cpp
+++ Primzahlen.cpp
@@ -77,25 +77,11 @@ int main()
             {
                 Anzahl++;
                 std::cout << "                                                    " << i << "\n";
-            }
-            
+            }   
         }
         std::cout << std::endl;
         std::cout << "                    Es gibt also " << Anzahl << " Primzahlen im Zahlenbereich von 1 bis " << Zahl << ".\n";
-
     }
-
-
-
 }
 
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
 
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
